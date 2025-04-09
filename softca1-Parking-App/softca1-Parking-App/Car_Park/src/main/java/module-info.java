bc9c8033fa72144736093ccae14c6fcbd217bf37
Michal_Lyra_C <147732616+Lyraa-05@users.noreply.github.com>
2025-04-09 13:49:12 +0100

    Add files via upload
    added listview to show list of reserved spaces and slighty updated GUI

---
+++ softca1-Parking-App/softca1-Parking-App/Car_Park/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example.car_park {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+//    requires com.dlsc.formsfx;
+
+    opens org.example.car_park to javafx.fxml;
+    exports org.example.car_park;
+}
